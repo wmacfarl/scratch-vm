7e3af5b18e34ef115dde9e73fe169eae9075559b
wmacfarl <wmacfarl@gmail.com>
2024-03-26 13:50:32 -0400

    add physics and bender

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -24,7 +24,8 @@ const builtinExtensions = {
     makeymakey: () => require('../extensions/scratch3_makeymakey'),
     boost: () => require('../extensions/scratch3_boost'),
     gdxfor: () => require('../extensions/scratch3_gdx_for'),
-    bender: () => require('../extensions/scratch3_bender')
+    bender: () => require('../extensions/scratch3_bender'),
+    physics: () => require('../extensions/scratch3_physics'),
 };
 
 /**
